7f8f3c703fcdc28ef78036967b5d35b11178eba9
Jonas David Rosa <jonasdavidrosa@hotmail.com>
2022-06-03 10:40:38 -0300

    ultima atualização

--- customer_classic..py
+++ customer_classic..py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv"
 dados= pd.read_csv(uri)
+SEED=20
 
 x= dados[["home","how_it_works","contact"]]
 print(x.head)
@@ -15,9 +17,11 @@ treino_x = x[:75]
 treino_y = y[:75]
 teste_x = x[75:]
 teste_y = y[75:]
-teste_y.shape
 
-print('treinaremos com %d elementos e testaremos com %d'% (len(treino_x),len(teste_x)))
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y,
+                                                        random_state = SEED, test_size = 0.25,
+                                                        stratify= y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
 
 modelo = LinearSVC()
 modelo.fit(treino_x, treino_y)
@@ -25,3 +29,6 @@ previsoes = modelo.predict(teste_x)
 
 acuracia = accuracy_score(teste_y, previsoes) * 100
 print("A acurácia foi %.2f%%" % acuracia)
+print(treino_y.value_counts())
+print(teste_y.value_counts())
+
